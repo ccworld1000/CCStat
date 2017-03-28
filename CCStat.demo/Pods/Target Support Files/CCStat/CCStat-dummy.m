71b35bc3a6a4853e6d1dd14472d59fae40a8626b
cc <ccworld1000@gmail.com>
2017-03-28 10:25:18 +0800

    ignore

--- CCStat.demo/Pods/Target Support Files/CCStat/CCStat-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_CCStat : NSObject
-@end
-@implementation PodsDummy_CCStat
-@end
--- CCStat.demo/Pods/Target Support Files/Pods-CCStat/Pods-CCStat-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_CCStat : NSObject
-@end
-@implementation PodsDummy_Pods_CCStat
-@end
